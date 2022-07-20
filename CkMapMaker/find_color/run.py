cb85bc2b445309cce372510f367fa5515e4b2c33
Imbar <imbrkvskh@gmail.com>
2022-07-20 17:43:18 +0500

    add map

--- CkMapMaker/find_color/run.py
+++ CkMapMaker/find_color/run.py
@@ -1,10 +1,11 @@
 from PIL import Image
 
-color = (4, 0, 33)
-img = Image.open("provinces.png")
+color = (35, 35, 35)
+img = Image.open("provinces.png").convert("RGB")
 
 width, height = img.size
 for x in range(0, width):
     for y in range(0, height):
+        r, g, b = img.getpixel((x, y))
         if img.getpixel((x, y)) == color:
             print(x, " ", y)
